cdc764df85824ada582b9554bf95ca94ada8cc1f
Nub_Dog <akhihithangngu@gmail.com>
2025-08-21 21:52:59 +0700

    xây xong sường cho side bar

--- src/components/ManagerFile_R2Storage/FileManager.tsx
+++ src/components/ManagerFile_R2Storage/FileManager.tsx
@@ -3,39 +3,53 @@ import './css/fileManager.css';
 import { useR2} from '../../hooks/useR2'; //hook này dùng để kết nối đến với R2
 
 const FileManager: React.FC = () => {
-  const {files, loading, fetchFiles} = useR2();
+  // const {files, loading, fetchFiles} = useR2();
 
-  useEffect(() => {
-    fetchFiles(); //lấy danh sách từ file R2
-  }, []);
+  // useEffect(() => {
+  //   fetchFiles(); //lấy danh sách từ file R2
+  // }, []);
 
-  if (loading) return (
-    <p>Đang tải sếp ơi chờ chút...</p>
-  );
+  // if (loading) return (
+  //   <p>Đang tải sếp ơi chờ chút...</p>
+  // );
 
-  if (files.length === 0) {
-    return (
-      <p>Không có file nào trong R2 Storage</p>
-    );
-  }
+  // if (files.length === 0) {
+  //   return (
+  //     <p>Không có file nào trong R2 Storage</p>
+  //   );
+  // }
 
   return (
     
     <div className= 'container_list_file'>
-      <h2>Danh sách file trong R2 Storage</h2>
+
+      <div className= "side_bar">
+        <div className= "side_bar_header">
+          <img src = "../../../public/logo.svg" className= "side_bar-header_logo"></img>
+        </div>
+
+        <div className= "side_bar_body"></div>
+
+        <div className= "side_bar_footer"></div>
+      </div>
+
+      <div className= "main_content"></div>
+
+
+      {/* <h2>Danh sách file trong R2 Storage</h2> */}
 
       {/*  nút này để reload lại danh sách file */}
-      <button
+      {/* <button
         onClick={fetchFiles}
         disabled={loading}
       >
         Tải lại danh sách
       </button>
 
-      <p>Tổng số file trong R2 Storage: {files.length}</p>
+      <p>Tổng số file trong R2 Storage: {files.length}</p> */}
 
       {/* Bắt đầu danh sách file */}
-      {files.map(
+      {/* {files.map(
         (file, index) => (
           <div key={index}>
             <h2>{file.key}</h2>
@@ -43,7 +57,7 @@ const FileManager: React.FC = () => {
             <p>Ngày upload: {new Date(file.uploaded).toLocaleString('vi-VN')}</p>
           </div>
         )
-      )}
+      )} */}
     </div>
   )
 
