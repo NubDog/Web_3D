bbc1657ab5f550356453c3121b073c858f0a3e0a
Nub_Dog <akhihithangngu@gmail.com>
2025-09-08 15:30:36 +0700

    Tạo file header và import vào

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './../styles/HomePage/HomePage.css';
+import '../index.css'
+import './../components/Logo/logo.tsx'
+
+// import các components ở đây
+import Header from './../components/Header/header.tsx'
 
 const HomePage = () => {
     return (
         <div className="home-page">
-            <h1>Home Page</h1>
+            <Header />
         </div>
     )
 }
