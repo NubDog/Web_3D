c1784cd11ae02e86a70b6b229e4cd2fdcee0fb21
Nub_Dog <akhihithangngu@gmail.com>
2025-09-08 15:44:41 +0700

    làm xong khung xường cho header dasboard đồng thời chỉnh lại kích thước của logo chính

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import './../../styles/Header/Header.css'
+import Logo from './../Logo/logo.tsx'
 
 const Header = () => {
     return (
-        <div className="header"></div>
+        <div className="header">
+            <Logo />
+            <ul className="header-menu">
+                <li className="header-menu_items"><a href="#">Cửa hàng</a></li>
+                <li className="header-menu_items"><a href="#">Ô tô</a></li>
+                <li className="header-menu_items"><a href="#">Xe máy</a></li>
+                <li className="header-menu_items"><a href="#">Xe đạp</a></li>
+                <li className="header-menu_items"><a href="#">Xe điện</a></li>
+                <li className="header-menu_items"><a href="#">Máy bay</a></li>
+                <li className="header-menu_items"><a href="#">Tàu thủy</a></li>
+                <li className="header-menu_items"><a href="#">Thiết bị quay phim</a></li>
+                <li className="header-menu_items"><a href="#">Hỗ trợ</a></li>
+                <li className="header-menu_items"><a href="#">Tìm kiếm</a></li>
+            </ul>
+
+            <div className="user-login">
+                <a href="#">Đăng nhập</a>
+                <a href="#">Đăng ký</a>
+            </div>
+        </div>
     )
 }
 
