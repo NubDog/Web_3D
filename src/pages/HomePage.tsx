e35d7295beb7865ec73c9d851c0902536fe992e7
Nub_Dog <akhihithangngu@gmail.com>
2025-09-08 15:24:46 +0700

    tạo thư mục page và tyles đồng thời tạo home page và file css

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../styles/HomePage/HomePage.css';
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <h1>Home Page</h1>
+        </div>
+    )
+}
+
+export default HomePage;
