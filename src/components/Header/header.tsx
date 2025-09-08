f6362179cfac930d50a2560d7c3a4a9a92b11ca9
Nub_Dog <akhihithangngu@gmail.com>
2025-09-09 01:34:38 +0700

    call thành công API và gọi về danh mục phương tiện

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './../../styles/Header/Header.css'
 import './../../styles/Header/Responsive.css'
 import Logo from './../Logo/logo.tsx'
 import Button from '../Button/Button.tsx'
 
+interface danhMucPhuongTien {
+    ten_danh_muc: string;
+}
+
 const Header = () => {
+    const [danhMucPhuongTien, setDanhMucPhuongTien] = useState<danhMucPhuongTien[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    const fetchDanhMucPhuongTien = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch('/api/Admin/danh-muc-phuong-tien');
+            if (!response.ok) throw new Error('response.statusText');
+            const json = await response.json();
+            if (!json.success) throw new Error(json.error || "Lỗi khi tải danh mục phương tiện");
+            setDanhMucPhuongTien(json.data);
+        } catch (error) {
+            console.error('Lỗi khi tải danh mục phương tiện:', error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchDanhMucPhuongTien();
+    }, []);
+
     return (
         <div className="header">
             <Logo />
             <ul className="header-menu">
                 <li className="header-menu_items"><a href="#">Cửa hàng</a></li>
-                <li className="header-menu_items"><a href="#">Ô tô</a></li>
-                <li className="header-menu_items"><a href="#">Xe máy</a></li>
-                <li className="header-menu_items"><a href="#">Xe đạp</a></li>
-                <li className="header-menu_items"><a href="#">Xe điện</a></li>
-                <li className="header-menu_items"><a href="#">Máy bay</a></li>
-                <li className="header-menu_items"><a href="#">Tàu thủy</a></li>
-                <li className="header-menu_items"><a href="#">Thiết bị quay phim</a></li>
+                
+                {danhMucPhuongTien.map((item, index) => (
+                    <li key={index} className="header-menu_items"><a href="#">{item.ten_danh_muc}</a></li>
+                ))}
+
                 <li className="header-menu_items"><a href="#">Hỗ trợ</a></li>
                 <li className="header-menu_items"><a href="#"><i className="fa-solid fa-magnifying-glass"></i></a></li>
             </ul>
