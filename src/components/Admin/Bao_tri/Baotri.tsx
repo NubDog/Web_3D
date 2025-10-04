d33ae071615d8ad0a32803953ddb94c21d37344a
NubDog <akhihithangngu@gmail.com~>
2025-10-04 13:26:13 +0700

    Sửa lỗi import file css [200~BaoTriList~

--- src/components/Admin/Bao_tri/Baotri.tsx
+++ src/components/Admin/Bao_tri/Baotri.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./Baotrilist.css";
+import './BaoTriList.css';
 import { useAuth } from "../../../contexts/AuthContext";
 
 interface BaoTriTongHop {
